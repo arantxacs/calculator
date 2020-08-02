5bbdf73e56806e6bb8b4847a6da8b82f22b982a4
Abner Colli <renba@Abners-MacBook-Pro.local>
2020-08-01 22:33:52 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,19 @@
+##### Prerequisites
+
+The setups for my calculator project in python
+- Python
+- Git
+
+##### 1. Check out the repository
+
+```bash
+git clone https://github.com/arantxacs/calculator.git
+```
+
+##### 2. How to Run
+
+After the installation in the terminal run:
+
+```bash
+py calculator.py
+```
--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 import sys
 
-# step 1 Say Hi and ask for operation.
+# Step 1 Say Hi and ask for operation.
 print("Hi user, this is your calculator")
 print("What operation you wanna do?")
 #Comand
@@ -9,11 +9,11 @@ print("- For the substraction (-)")
 print("- For the multiplication (*)")
 print("- For the Division (/)")
 
-# step 2 read and whait for the answer
+# Step 2 read and whait for the answer
 answer = input()
 print(f'Your answer is {answer}')
 
-#step 3 parse and decide answer
+# Step 3 parse and decide answer
 if "+" == answer:
     print("you want to do a sum")
 elif "-" == answer:   
